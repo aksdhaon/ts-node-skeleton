b90c5ce27876c1aba0ad176329f56e1d6e47933b
aksdhaon <akshat.dhaon@live.com>
2026-03-26 15:35:25 +0000

    Test push

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { type Request, type Response } from "express";
 
 export const app = express();
 
+// check app health
 app.get("/health", (_req: Request, res: Response) => {
   res.status(200).json({ status: "ok" });
 });
